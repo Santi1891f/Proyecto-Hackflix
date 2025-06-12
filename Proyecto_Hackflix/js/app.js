e43a6eb800d5c7bece9d3cd72342a07801159bd8
MarceEscobal <marcelo.escobal@anima.edu.uy>
2025-06-12 19:39:25 -0300

    v1

--- Proyecto_Hackflix/js/app.js
+++ Proyecto_Hackflix/js/app.js
@@ -1,51 +1,27 @@
-const pelicula=document.querySelectortor (".col-md-3");
-const portada = document.querySelector ("");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const htmlElement = document.documentElement;
+  const switchElement = document.getElementById("darkModeSwitch");
 
-    const foto = portada.value.trim();
+  // Set the default theme to dark if no setting is found in local storage
+  const currentTheme = localStorage.getItem("bsTheme") || "dark";
+  htmlElement.setAttribute("data-bs-theme", currentTheme);
+  switchElement.checked = currentTheme === "dark";
 
+  switchElement.addEventListener("change", function () {
+    if (this.checked) {
+      htmlElement.setAttribute("data-bs-theme", "dark");
+      localStorage.setItem("bsTheme", "dark");
+    } else {
+      htmlElement.setAttribute("data-bs-theme", "light");
+      localStorage.setItem("bsTheme", "light");
+    }
+  });
+});
 
+const pelicula = document.querySelectortor(".col-md-3");
+const portada = document.querySelector("");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const foto = portada.value.trim();
 
 // const buscar = document.querySelector("#btn-form");
 // const pelicula = document.querySelector("#peli");
